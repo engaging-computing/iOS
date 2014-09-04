2ee664ee7c9719e8566697fa2274ea7bfa3abfdf
stowellm <stowellmichael@gmail.com>
2014-09-04 17:14:23 -0400

    Added API test application to iOS\Test repository

---
+++ Tests/APITest/APITest/ATAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ATAppDelegate.h
+//  APITest
+//
+//  Created by Mike Stowell on 9/4/14.
+//  Copyright (c) 2014 iSENSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tests/APITest/APITest/ATViewController.h
@@ -0,0 +1,27 @@
+//
+//  ATViewController.h
+//  APITest
+//
+//  Created by Mike Stowell on 9/4/14.
+//  Copyright (c) 2014 iSENSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "../../../Libs/iSENSE_API/iSENSE_API/API.h"
+
+@interface ATViewController : UIViewController <UITextFieldDelegate> {
+    API *api;
+}
+
+@property (strong, nonatomic) IBOutlet UITextField *emailInputTxt;
+@property (strong, nonatomic) IBOutlet UITextField *passInputTxt;
+@property (strong, nonatomic) IBOutlet UIButton *loginBtn;
+- (IBAction)loginBtnClick:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UILabel *loginOutLbl;
+
+@property (strong, nonatomic) IBOutlet UIButton *testAllBtn;
+- (IBAction)testAllBtnClick:(id)sender;
+@property (strong, nonatomic) IBOutlet UITextView *testAllOutLbl;
+
+@end
