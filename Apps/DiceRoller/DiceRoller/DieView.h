64efbb9ef8eabb2b587c5e3f6eddf3e7f422a5e6
Rajia <rajia@polaris-2.local>
2014-09-30 15:35:34 -0400

    Dice Roller App with UI

---
+++ Apps/DiceRoller/DiceRoller/DieView.h
@@ -0,0 +1,13 @@
+//
+//  DieView.h
+//  DiceRoller
+//
+//  Created by Rajia  on 9/30/14.
+//  Copyright (c) 2014 iSENSE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieView : UIView
+
+@end
--- Apps/DiceRoller/DiceRoller/ViewController.h
+++ Apps/DiceRoller/DiceRoller/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DieView.h"
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *rollButton;
+@property (weak, nonatomic) IBOutlet UILabel *sumLabel;
+@property (weak, nonatomic) IBOutlet DieView *firstDieView;
+@property (weak, nonatomic) IBOutlet DieView *secondDieView;
 
 @end
