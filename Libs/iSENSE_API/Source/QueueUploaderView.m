dad5a7e499975a77252a6a04526add677fc8ffc2
stowellm <stowellmichael@gmail.com>
2014-10-24 12:51:16 -0400

    ... we now delete cells from the table.  Like we should have been doing.  All the time.

--- Libs/iSENSE_API/Source/QueueUploaderView.m
+++ Libs/iSENSE_API/Source/QueueUploaderView.m
@@ -176,8 +176,11 @@
 
         // Delete the row from the data source
         cell = (QueueCell *) [self.mTableView cellForRowAtIndexPath:indexPath];
+
         [limitedTempQueue removeObjectForKey:[cell getKey]];
         [dataSaver removeDataSet:[cell getKey]];
+
+        [self.mTableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
         [self.mTableView reloadData];
     }
 }
