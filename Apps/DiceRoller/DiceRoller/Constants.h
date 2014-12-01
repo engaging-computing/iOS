238d674142a03ec664efd326ecc5d0ea7119d6b7
Rajia <rajia@polaris-2.local>
2014-12-01 12:07:25 -0500

    Implemented Project Manager

---
+++ Apps/DiceRoller/DiceRoller/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  DiceRoller
+//
+//  Created by Rajia  on 12/1/14.
+//  Copyright (c) 2014 iSENSE. All rights reserved.
+//
+
+#ifndef DiceRoller_Constants_h
+#define DiceRoller_Constants_h
+
+// Project constants
+#define kNO_PROJECT @"Not Set"
+
+#endif
